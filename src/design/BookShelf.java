1c869ce15209afe64234e1cd5c202c03110e525c
pexIDEA <1130663011@qq.com>
2023-02-14 21:08:33 +0800

    优化书架类的数据结构

--- src/design/BookShelf.java
+++ src/design/BookShelf.java
@@ -4,6 +4,25 @@ public class BookShelf implements Aggregate {
 
     private Book[] books;
 
+    /*
+     * 优化Books结构，将数组改为ArrayList
+     * */
+
+//    private ArrayList bookArrayList;
+//    public BookShelf(int initialSize){
+//        this.bookArrayList = new ArrayList(initialSize);
+//    }
+//    public Book getBookFromArrayList(int index){
+//        return (Book) bookArrayList.get(index);
+//    }
+//    public boolean appendBookArrayList(Book book){
+//        return bookArrayList.add(book);
+//    }
+//    public int getArrayListLength(){
+//        return bookArrayList.size();
+//    }
+
+
     private int last = 0;
 
     public BookShelf(int maxsize) {
@@ -18,6 +37,7 @@ public class BookShelf implements Aggregate {
         this.books[last] = book;
         last++;
     }
+
 
     public int getLength() {
         return last;
